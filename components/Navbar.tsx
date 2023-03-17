da51a080e3fbaf1eb1aa9471c07ca4351882d539
diinhill <hillenbrand.nadine@hotmail.de>
2023-03-17 07:14:20 +0100

    new header design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,10 @@ export default function Navbar() {
         <div className='sticky top-0 w-screen rounded h-12 md:h-12 lg:h-14 bg-white overflow-hidden z-10'>
           <ul className='flex flex-row justify-center items-center space-x-6 text-black text-2xl text-center list-none h-12 md:h-12 lg:h-14'>
             <li className=''>
-              <Link className='no-underline font-[Kranky] hover:text-green-400 active:text-red-400' href='#menu' scroll={false}>menu</Link>
+              <Link className='no-underline font-["Coming_Soon"] hover:text-green-400 active:text-red-400' href='#menu' scroll={false}>menu</Link>
             </li>
             <li className=''>
-              <Link className='no-underline font-[Kranky] hover:text-green-400 active:text-red-400' href='#contact' scroll={false}>how to find us</Link>
+              <Link className='no-underline font-["Comic_Neue"] hover:text-green-400 active:text-red-400' href='#contact' scroll={false}>how to find us</Link>
             </li>
              <li className=''>
               <Link className='no-underline font-[Kranky] hover:text-green-400 active:text-red-400' href='#about' scroll={false}>about</Link>
