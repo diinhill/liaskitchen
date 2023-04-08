12312a73b64dc283433d0641f5fe2b31545fb958
diinhill <hillenbrand.nadine@hotmail.de>
2023-04-08 19:02:06 +0200

    cards styling

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -25,14 +25,19 @@ const MenuItem = ( item: MenuItem ) => {
                 <div className='ml-2'>
                     <h2 className='text-3xl font-["Annie_Use_Your_Telescope"] text-white'>{item.item.name}</h2>
                     <h3 className='text-2xl font-["Annie_Use_Your_Telescope"] text-white'>{item.item.price}</h3>
-
-                    <div className={styles.containerButton}>
-                        <a className={styles.button} href=''>
-                        </a>
-				    </div> 
-
-                    {/* {item.item.description != '' ? <p className='text-right pb-6 pt-2 pl-12 pr-6 font-["Coming_Soon"] text-blue-600'>{item.item.description}</p> : <p className='pb-0'></p>} */}
                 </div>
+
+                <div className={styles.dropdown}>
+                    <div className={styles.containerButton}>
+                        <button className={styles.button} />
+                    </div> 
+                    <div className={styles.dropdownContent}>
+                        <div className='bg-[#7ECBE9] rounded-sm shadow-[#366477] shadow-md'>
+                            <p className='text-2xl m-2 p-2 text-white font-["Annie_Use_Your_Telescope"]'>{item.item.description}</p>
+                        </div>
+                    </div>
+                </div>
+               
             </div>
         </>   
     )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,62 +6,40 @@ export default function Navbar() {
     return(
       <>
 			<div id='navbar' className='sticky top-0 bg-[#fffff6] z-10'>
-		{/* <div id='navbar' className='sticky top-0 border-b-2 border-b-black rounded h-16 md:h-20 lg:h-24 bg-[#fffff6] z-10'> */}
-
-          <div className={styles.dropdown}>
-			<button className={styles.button}>
-						
-				<svg x="0px" y="0px"
-					width="20%" height="20%" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xmlSpace="preserve">
-				<circle fill-rule="evenodd" clip-rule="evenodd" fill="#7ECBE9" stroke="#000000" stroke-width="3" stroke-miterlimit="10" cx="16.34" cy="47.577" r="13.303"/>
-				<circle fill-rule="evenodd" clip-rule="evenodd" fill="#7ECBE9" stroke="#000000" stroke-width="3" stroke-miterlimit="10" cx="50.062" cy="47.577" r="13.303"/>
-				<circle fill-rule="evenodd" clip-rule="evenodd" fill="#7ECBE9" stroke="#000000" stroke-width="3" stroke-miterlimit="10" cx="83.783" cy="47.577" r="13.302"/>
-				</svg>
-		
-			</button>
-            <div className={styles.dropdownContent}>
-              <ul className='text-[#7ECBE9] bg-[#fffff6] text-2xl text-center list-none'>
-                <li className=''>
-								<Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
-									href='#menu' scroll={false}>menu</Link>
-                </li>
-                <li className=''>
-								<Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
-									href='#contact' scroll={false}>how to find us</Link>
-                </li>
-                <li className=''>
-								<Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
-									href='#about' scroll={false}>reservation</Link>
-                </li>
-                <li className=''>
-								<Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
-									href='#about' scroll={false}>delivery</Link>
-                </li>
-              </ul>
-            </div>
+        <div className={styles.dropdown}>
+            
+			    <button className={styles.button}>
+            <svg x="0px" y="0px"
+              width="20%" height="20%" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xmlSpace="preserve">
+            <circle fill-rule="evenodd" clip-rule="evenodd" fill="#7ECBE9" stroke="#000000" stroke-width="3" stroke-miterlimit="10" cx="16.34" cy="47.577" r="13.303"/>
+            <circle fill-rule="evenodd" clip-rule="evenodd" fill="#7ECBE9" stroke="#000000" stroke-width="3" stroke-miterlimit="10" cx="50.062" cy="47.577" r="13.303"/>
+            <circle fill-rule="evenodd" clip-rule="evenodd" fill="#7ECBE9" stroke="#000000" stroke-width="3" stroke-miterlimit="10" cx="83.783" cy="47.577" r="13.302"/>
+            </svg>
+          </button>
+            
+          <div className={styles.dropdownContent}>
+            <ul className='text-[#7ECBE9] bg-[#fffff6] text-2xl text-center list-none'>
+              <li className=''>
+              <Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
+                href='#menu' scroll={false}>menu</Link>
+              </li>
+              <li className=''>
+              <Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
+                href='#contact' scroll={false}>how to find us</Link>
+              </li>
+              <li className=''>
+              <Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
+                href='#about' scroll={false}>reservation</Link>
+              </li>
+              <li className=''>
+              <Link className='z-30 no-underline font-["Annie_Use_Your_Telescope"] hover:text-[#a00242] active:text-[#a00242] hover:text-4xl active:text-4xl '
+                href='#about' scroll={false}>delivery</Link>
+              </li>
+            </ul>
+          </div>
 				</div>  
-
-
-          {/* <ul className='flex flex-row justify-center items-center space-x-6 text-black text-2xl text-center list-none h-12 md:h-12 lg:h-14'>
-            <li className=''>
-              <Link className='z-30 no-underline font-["Coming_Soon"] hover:text-green-400 active:text-red-400' href='#menu' scroll={false}>menu</Link>
-            </li>
-            <li className=''>
-              <Link className='z-30 no-underline font-["Coming_Soon"] hover:text-green-400 active:text-red-400' href='#contact' scroll={false}>how to find us</Link>
-            </li>
-             <li className=''>
-              <Link className='z-30 no-underline font-["Coming_Soon"] hover:text-green-400 active:text-red-400' href='#about' scroll={false}>reservation</Link>
-            </li>
-             <li className=''>
-              <Link className='z-30 no-underline font-["Coming_Soon"] hover:text-green-400 active:text-red-400' href='#about' scroll={false}>delivery</Link>
-            </li>
-          </ul> */}
-
-
-
-
-        </div>
-        </>
+      </div>
+      </>
     )
 }
 
