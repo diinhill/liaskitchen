f26919ad285b5146719a5c7ebd9eac2650c32769
Lucas Dupias <lucas@codeacademyberlin.com>
2023-04-17 02:38:41 +0200

    impressum page / no styling

---
+++ pages/impressum.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Menu from "../components/Menu";
+import Footer from "../components/Footer";
+import { useEffect, useRef } from "react";
+import Impressum from "@/components/Impressum";
+
+export default function ImpressumPage() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.noiseTexture}>
+        <Navbar />
+
+        <Impressum />
+        <Footer />
+      </div>
+
+      <div className={styles.containerBackToTopButton}>
+        <a className={styles.backToTopButton} href="#" />
+      </div>
+    </main>
+  );
+}
+export const getStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
